e1055679ae68850adbef9b9834c41a1d2bef34da
Shourov <shourov.ahmed@student.ap.be>
2020-06-08 02:46:08 +0200

    Klasse speelkaart aangemaakt

---
+++ Kaarten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaarten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kaarten/Speelkaart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaarten
+{
+    enum Suite { Schoppen, Harten, Klaveren, Ruiten}
+    class Speelkaart
+    {
+        public Speelkaart(int get, Suite kleur)
+        {
+            Nummer = get;
+            kleur = Kleur;
+        }
+
+        public int Nummer { get; set; }
+        public Suite Kleur { get; set; }
+    }
+}
