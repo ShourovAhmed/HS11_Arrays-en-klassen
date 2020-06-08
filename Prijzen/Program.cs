66ff078dee69e1e2da890c86c8422d4bae8f6a85
Shourov <shourov.ahmed@student.ap.be>
2020-06-08 02:42:19 +0200

    Prijzen oef af + getest

--- Prijzen/Program.cs
+++ Prijzen/Program.cs
@@ -6,7 +6,26 @@ namespace Prijzen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] prijzen = new double[20];
+            
+            for (int i = 0; i < prijzen.Length; i++)
+            {
+                Console.WriteLine("Voer een prijs in:");
+                prijzen[i] = Convert.ToDouble(Console.ReadLine()); 
+            }
+            Console.WriteLine("\n");
+
+            double tot = 0;
+            Console.WriteLine("Prijzen groter of gelijk aan 5:");
+            foreach (var prijs in prijzen)
+            {
+                tot += prijs;
+                if (prijs >= 5)
+                    Console.WriteLine(prijs);
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"Gemiddelde: {tot/prijzen.Length}");
         }
     }
 }
