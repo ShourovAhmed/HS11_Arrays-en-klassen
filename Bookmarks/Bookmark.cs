ded10a1f390b7fdb2fa137f846a8b6fd6913fcfb
Shourov <shourov.ahmed@student.ap.be>
2020-06-08 03:11:30 +0200

    bookmarks oef gestart + klasse van prof toegevoegd

---
+++ Bookmarks/Bookmark.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Bookmarks
+{
+    class Bookmark
+    {
+        class BookMark
+        {
+
+            public string Naam { get; set; }
+            public string URL { get; set; }
+            public void OpenSite()
+            {
+                Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", URL);  //Voeg bovenaan using System.Diagnostics; toe
+
+            }
+        }
+    }
+}
